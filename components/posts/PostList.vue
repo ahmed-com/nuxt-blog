144e59c3b417bed7e9e020ee9bc32cdc24cbc7e4
ahmed-com <52245980+ahmed-com@users.noreply.github.com>
2020-05-30 04:35:30 +0200

    added the first draft

---
+++ components/posts/PostList.vue
@@ -0,0 +1,32 @@
+<template>
+    <section class="post-list">
+      <PostPreview :isAdmin="isAdmin" id="1" title="Here will go the title" previewText="Here will go the preview text" thumbnail="https://www.vapulus.com/en/wp-content/uploads/2019/01/tech.jpg"/>
+    </section>
+</template>
+<script>
+import PostPreview from '@/components/posts/PostPreview'
+
+export default {
+  props : {
+    isAdmin : {
+      type : Boolean,
+      default : false
+    }
+  },
+  components : {
+    PostPreview
+  }
+}
+</script>
+
+<style scoped>
+
+.post-list {
+  display: flex;
+  padding: 20px;
+  box-sizing: border-box;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+}
+</style>
---
+++ pages/admin/_postId/index.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="admin-post-page">
+        <section class="update-form">
+            <AdminPostForm :post="loadedPost" />
+        </section>
+    </div>
+</template>
+<script>
+import AdminPostForm from '@/components/admin/AdminPostForm';
+
+export default {
+    layout : 'admin',
+    components : {
+        AdminPostForm
+    },
+    data(){
+        return {
+            loadedPost : {
+                author : 'ahmed',
+                title : 'this will be a title in the end',
+                content : 'this will be also some content',
+                thumbnailLink : 'https://www.vapulus.com/en/wp-content/uploads/2019/01/tech.jpg'
+            }
+        }
+    }
+}
+</script>
+<style scoped>
+.update-form {
+  width: 90%;
+  margin: 20px auto;
+}
+@media (min-width: 768px) {
+  .update-form {
+    width: 500px;
+  }
+}
+</style>
